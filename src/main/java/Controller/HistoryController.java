7e14da3d4452918f47c9241094e9007de2511268
ducthong230497 <15520856@gm.uit.edu.vn>
2018-06-13 20:50:18 +0700

    add search history + refresh + fix a bug when deleting history

--- src/main/java/Controller/HistoryController.java
+++ src/main/java/Controller/HistoryController.java
@@ -7,6 +7,7 @@ import Model.SqliteDatabase.SQLiteDatabase;
 import com.jfoenix.controls.*;
 import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
 import com.sun.istack.internal.NotNull;
+import com.sun.org.apache.xpath.internal.SourceTree;
 import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import javafx.animation.PathTransition;
 import javafx.animation.Timeline;
@@ -41,10 +42,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class HistoryController implements Initializable, IReferencable {
     public static final String FXMLPATH = "/View/History.fxml";
@@ -67,6 +65,11 @@ public class HistoryController implements Initializable, IReferencable {
     @FXML
     private Button deleteButton;
 
+    @FXML
+    private TextField txbSearch;
+
+    @FXML Button btnRefresh;
+
     JFXTreeTableColumn<HistoryView, String> dateCol = new JFXTreeTableColumn<HistoryView, String>("Date");
 
     JFXTreeTableColumn<HistoryView, String> linkCol = new JFXTreeTableColumn<HistoryView, String>("Link");
@@ -79,6 +82,8 @@ public class HistoryController implements Initializable, IReferencable {
 
     TreeItem<HistoryView> root;
 
+    List<TreeItem<HistoryView>> listRoot = new ArrayList<>();
+
     TranslateTransition actionBarTransition, searchBarTransition;
 
     @Override
@@ -88,52 +93,8 @@ public class HistoryController implements Initializable, IReferencable {
         }
         //region add columns and data
         addColumns();
-        /*try{
 
-            FileReader fileReader = new FileReader("history.txt");
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            String line;
-            while ((line = bufferedReader.readLine())!=null){
-                String[] temp = line.split("´");
-                HistoryView historyView = new HistoryView(temp[1], temp[0], temp[2], temp[4], temp[3]);
-                root.getChildren().add(new TreeItem<>(historyView));
-            }
-        }
-        catch (IOException e){
-            System.out.println(getClass().getSimpleName());
-            e.printStackTrace();
-        }*/
-        try {
-            Statement statement = SQLiteDatabase.getInstance().getConnection().createStatement();
-            PreparedStatement preparedStatement = SQLiteDatabase.getInstance().getConnection().prepareStatement("SELECT name FROM sqlite_master WHERE type=?");
-            preparedStatement.setString(1, "table");
-            ResultSet tables = preparedStatement.executeQuery();
-            while (tables.next()){
-                String name = tables.getString("name");
-                String table = name.substring(5).substring(0,2) + "-" + name.substring(5).substring(2);
-                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM-yyyy");
-                root = new RecursiveTreeItem<HistoryView>(new HistoryView(table, "", "", "", ""),
-                        RecursiveTreeObject::getChildren);
-                System.out.println(root.getValue().date);
-                tbvHistory.getRoot().getChildren().add(root);
-                ResultSet resultSet = statement.executeQuery("SELECT * FROM " + name);
-                while (resultSet.next()) {
-                    String url = resultSet.getString("url");
-                    String date = resultSet.getString("accessdate");
-                    String time = resultSet.getString("accesstime");
-                    String title = resultSet.getString("title");
-                    String domain = resultSet.getString("domain");
-                    HistoryView historyView = new HistoryView(date, url, time, domain, title);
-                    root.getChildren().add(new TreeItem<>(historyView));
-                }
-            }
-
-        } catch (SQLException e) {
-            System.out.println("History controller initialize");
-            e.printStackTrace();
-        } finally {
-            SQLiteDatabase.getInstance().Disconnect();
-        }
+        addDataToTreeTableView();
         //endregion
 
         //region actionBar transition
@@ -162,6 +123,10 @@ public class HistoryController implements Initializable, IReferencable {
 
         gotoWebsiteButton.setOnMouseClicked(e -> gotoWebSite());
         deleteButton.setOnMouseClicked(e -> deleteHistory());
+
+        txbSearch.textProperty().addListener(e->SearchChange());
+
+        btnRefresh.setOnAction(e->RefreshHistory());
     }
 
     private void addColumns() {
@@ -222,6 +187,35 @@ public class HistoryController implements Initializable, IReferencable {
         tbvHistory.setShowRoot(false);
     }
 
+    private void addDataToTreeTableView() {
+        try {
+            Statement statement = SQLiteDatabase.getInstance().getConnection().createStatement();
+            PreparedStatement preparedStatement = SQLiteDatabase.getInstance().getConnection().prepareStatement("SELECT name FROM sqlite_master WHERE type=?");
+            preparedStatement.setString(1, "table");
+            ResultSet tables = preparedStatement.executeQuery();
+            while (tables.next()){
+                String name = tables.getString("name");
+                String table = name.substring(5).substring(0,2) + "-" + name.substring(5).substring(2);
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM-yyyy");
+                root = new RecursiveTreeItem<HistoryView>(new HistoryView(table, "", "", "", ""),
+                        RecursiveTreeObject::getChildren);
+                //System.out.println(root.getValue().date);
+                tbvHistory.getRoot().getChildren().add(root);
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM " + name);
+                while (resultSet.next()) {
+                    AddDataToRow(resultSet, root);
+                }
+                listRoot.add(root);
+            }
+
+        } catch (SQLException e) {
+            System.out.println("History controller initialize");
+            e.printStackTrace();
+        } finally {
+            SQLiteDatabase.getInstance().Disconnect();
+        }
+    }
+
     public JFXTreeTableView getTbvHistory() {
         return tbvHistory;
     }
@@ -253,42 +247,132 @@ public class HistoryController implements Initializable, IReferencable {
 
     private void deleteHistory() {
         ObservableList<TreeItem<HistoryView>> histories = tbvHistory.getSelectionModel().getSelectedItems();
-        for (TreeItem<HistoryView> item : histories) {
-            String url = item.getValue().link.getValue();
-            String accessDate = item.getValue().date.getValue();
-            String accessTime = item.getValue().time.getValue();
-            if (url.equals("")) {
-                System.out.println("It's root");
-                continue;
-            }
-            try {
-                //region add to DB
-                PreparedStatement preparedStatement = SQLiteDatabase.getInstance().getConnection().prepareStatement("DELETE FROM " + SQLiteDatabase.getInstance().getTableName() + " WHERE url=? AND accessdate = ? AND accesstime = ?");
+        try {
+            for (TreeItem<HistoryView> item : histories) {
+                String url = item.getValue().link.getValue();
+                if (url.equals("")) {
+                    System.out.println("It's root");
+                    continue;
+                }
+                String accessDate = item.getValue().date.getValue();
+                String accessTime = item.getValue().time.getValue();
+
+
+                TreeItem<HistoryView> parent = item.getParent();
+                String []temp = parent.getValue().date.getValue().split("-");
+                String table = "month"+temp[0]+temp[1];
+                //parent.getValue().
+                //region delete from DB
+                PreparedStatement preparedStatement = SQLiteDatabase.getInstance().getConnection().prepareStatement("DELETE FROM " + table + " WHERE url=? AND accessdate = ? AND accesstime = ?");
                 preparedStatement.setString(1, url);
                 preparedStatement.setString(2, accessDate);
                 preparedStatement.setString(3, accessTime);
                 preparedStatement.executeUpdate();
                 //endregion
                 System.out.println("successfully delete in db");
-            } catch (SQLException e) {
-                System.err.println(e.getMessage());
-            } finally {
-                SQLiteDatabase.getInstance().Disconnect();
             }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            SQLiteDatabase.getInstance().Disconnect();
         }
+
         List<TreeItem<HistoryView>> nodes = tbvHistory.getSelectionModel().getSelectedItems();
-        int n = nodes.size();
+        int n = histories.size();
         while(n > 0){
-            TreeItem<HistoryView> temp = nodes.get(0);
-            TreeItem<HistoryView> parent = nodes.get(0).getParent();
+            TreeItem<HistoryView> temp = nodes.get(n - 1);
+            if(temp.getValue().link.getValue().equals("")){
+                n--;
+                continue;
+            }
+            TreeItem<HistoryView> parent = temp.getParent();
             if (parent != null){
-                parent.getChildren().remove(nodes.get(0));
+                parent.getChildren().remove(temp);
                 n--;
             }
             else {
                 return;
             }
         }
+        tbvHistory.getSelectionModel().clearSelection();
+        //tbvHistory.getSelectionModel().getSelectedItems().clear();
+    }
+
+    private void SearchChange(){
+        if(txbSearch.getText().length() == 0){
+            tbvHistory.getRoot().getChildren().clear();
+            for (TreeItem<HistoryView> item : listRoot) {
+                tbvHistory.getRoot().getChildren().add(item);
+            }
+        }
+        else {
+            tbvHistory.getRoot().getChildren().clear();
+            PreparedStatement preparedStatementTable = null;
+            PreparedStatement preparedStatementData = null;
+            try {
+                preparedStatementTable = SQLiteDatabase.getInstance().getConnection().prepareStatement("SELECT name FROM sqlite_master WHERE type=?");
+                preparedStatementTable.setString(1, "table");
+                ResultSet tables = preparedStatementTable.executeQuery();
+                while (tables.next()){
+                    String name = tables.getString("name");
+                    preparedStatementData = SQLiteDatabase.getInstance().getConnection().prepareStatement("SELECT * FROM " + name + " WHERE url LIKE ?");
+                    preparedStatementData.setString(1, "%" + txbSearch.getText() + "%");
+                    ResultSet resultSet = preparedStatementData.executeQuery();
+                    while (resultSet.next()){
+                        AddDataToRow(resultSet, tbvHistory.getRoot());
+                    }
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            finally {
+                SQLiteDatabase.getInstance().Disconnect();
+            }
+        }
+    }
+
+    private void RefreshHistory(){
+        Statement statementTable = null;
+        Statement statementData = null;
+        try {
+            statementTable = SQLiteDatabase.getInstance().getConnection().createStatement();
+            ResultSet resultSet = statementTable.executeQuery("SELECT name FROM sqlite_master where type = 'table'");
+            String table = "";
+            while (resultSet.next()){
+                 table = resultSet.getString("name");
+                System.out.println(table);
+            }
+            if(!table.equals("")){
+                String name = table.substring(5).substring(0,2) + "-" + table.substring(5).substring(2);
+                statementData = SQLiteDatabase.getInstance().getConnection().createStatement();
+                ResultSet resultSetData = statementData.executeQuery("SELECT * FROM " + table);
+                TreeItem<HistoryView> root = new RecursiveTreeItem<HistoryView>(new HistoryView(name, "", "", "", ""),
+                        RecursiveTreeObject::getChildren);
+                listRoot.remove(listRoot.size()-1);
+                tbvHistory.getRoot().getChildren().remove(tbvHistory.getRoot().getChildren().size() - 1);
+                //root.getChildren().removeAll();
+                while (resultSetData.next()){
+                    AddDataToRow(resultSetData, root);
+                }
+                listRoot.add(root);
+                tbvHistory.getRoot().getChildren().add(root);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+//        int n = tbvHistory.getRoot().getChildren().size();
+//        tbvHistory.getRoot().getChildren().remove(n - 1);
+    }
+
+    private void AddDataToRow(ResultSet resultSet, TreeItem<HistoryView> root) throws SQLException {
+        String url = resultSet.getString("url");
+        String date = resultSet.getString("accessdate");
+        String time = resultSet.getString("accesstime");
+        String title = resultSet.getString("title");
+        String domain = resultSet.getString("domain");
+        HistoryView historyView = new HistoryView(date, url, time, domain, title);
+        root.getChildren().add(new TreeItem<>(historyView));
     }
 }
 class HistoryView extends RecursiveTreeObject<HistoryView>{
