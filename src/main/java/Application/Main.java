ff29dd87191d00190a1630061e9e143ef499dd01
ducthong230497 <15520856@gm.uit.edu.vn>
2018-06-14 16:42:59 +0700

    quite done

--- src/main/java/Application/Main.java
+++ src/main/java/Application/Main.java
@@ -4,9 +4,11 @@ import Model.SqliteDatabase.SQLiteDatabase;
 import Model.SqliteDatabase.SqliteDatabaseBookmarks;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -17,7 +19,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        //region connect to mysql
+        //region connect to DB
         /*try {
             Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException e) {
@@ -31,10 +33,13 @@ public class Main extends Application {
         bookmarksDB.ConnectToDatabase();
         //boolean TableExists = webHistoryDB.CreateTableIfUnexists();
         //endregion
+        Screen screen = Screen.getPrimary();
+        Rectangle2D bounds = screen.getVisualBounds();
         Parent root = FXMLLoader.load(getClass().getResource("/View/tabpane.fxml"));
         primaryStage.setTitle("Clover Browser");
-        primaryStage.setScene(new Scene(root, 1200, 600));
+        primaryStage.setScene(new Scene(root, bounds.getWidth() - 100, bounds.getHeight() - 100));
         primaryStage.getIcons().add(new Image("../resources/Drawable/three_leaf_clover.png"));
+        //primaryStage.setMaximized(true);
         currentStage = primaryStage;
         primaryStage.show();
     }
